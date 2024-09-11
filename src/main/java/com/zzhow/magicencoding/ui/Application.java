9807dc01866f1f9028d60d11ceb853a6c6c2e30f
ZZHow <109335896+ZZHow1024@users.noreply.github.com>
2024-09-11 23:53:48 +0800

    feat: Add new UI interface

--- src/main/java/com/zzhow/magicencoding/ui/Application.java
+++ src/main/java/com/zzhow/magicencoding/ui/Application.java
@@ -23,7 +23,7 @@ public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 535);
+        Scene scene = new Scene(fxmlLoader.load(), 780, 596);
         stage.setTitle("MagicEncoding");
         stage.setScene(scene);
         Image icon = new Image(Objects.requireNonNull(MainClass.class.getResourceAsStream("/image/icon.png")));
